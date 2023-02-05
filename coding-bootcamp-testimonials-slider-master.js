2cff1ba0e4a6573f1a57727622372bf88f66f12b
red0ahlam <109111970+red0ahlam@users.noreply.github.com>
2023-02-05 17:28:52 +0100

    Add files via upload

---
+++ coding-bootcamp-testimonials-slider-master.js
@@ -0,0 +1,105 @@
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.previous');
+const review = document.querySelector('.paragraph');
+const reviewerName = document.querySelector('.reviewer-name');
+const reviewerJob = document.querySelector('.reviewer-job');
+const reviewerImage = document.querySelector('.reviewer');
+const carouselText = document.querySelector('.carousel-text');
+const carouselImage = document.querySelector('.carousel-image');
+const carousel = document.querySelector('.carousel');
+let slide = 0;
+
+let images = ["images/image-tanya.jpg","images/image-john.jpg"];
+
+// function changeText (slide,reviewers) {
+//     carouselImage.classList.remove('slidein');
+//     carouselImage.classList.remove('slideout');
+
+//     carouselImage.classList.add('slidein');
+//     carouselText.classList.add('opacity-zero');
+//     carouselImage.addEventListener('animationend', ()=> {
+//         carouselImage.classList.remove('slidein');
+//         carouselImage.classList.add('slideout');
+//     },{once: true});
+//     setTimeout(()=> {
+//         review.innerHTML = ` “ ${reviewers[slide].review} ”` ;
+//         reviewerName.innerHTML = reviewers[slide].name;
+//         reviewerJob.innerHTML = reviewers[slide].job;
+//         reviewerImage.setAttribute('src',images[slide]);
+//         carouselText.classList.remove('opacity-zero');
+//     },"750");
+//     setTimeout(() => {
+//         carouselImage.addEventListener('animationend', ()=> {
+//             carouselImage.classList.remove('slideout');
+//         },{once: true});
+//     }, 800);
+    
+// };
+
+
+function animate (animated,animation,duration) {
+    animated.classList.add(animation);
+    setTimeout(()=> {
+        animated.classList.remove(animation); 
+    }, duration)
+}
+
+function changeText (slide,reviewers) {
+    animate(carouselText,"opacity-zero",400);
+    animate(carouselImage,"scaleup",400);
+    review.innerHTML = ` “ ${reviewers[slide].review} ”` ;
+    reviewerName.innerHTML = reviewers[slide].name;
+    reviewerJob.innerHTML = reviewers[slide].job;
+    reviewerImage.setAttribute('src',images[slide]);
+    reviewerImage.setAttribute('alt',`image of ${reviewers[slide].name}`)
+    carousel.setAttribute('aria-label', ` slide ${slide+1} of 2`)
+}
+
+nextButton.addEventListener('click', ()=> {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET','reviewers.json',true);
+
+    xhr.onload = function() {
+        if(this.status == 200) {
+            let data = this.responseText;
+            data = JSON.parse(data);
+            let reviewers = data.reviewers;
+            
+            if (reviewers.length == (slide + 1)) {
+                slide = 0;
+                changeText (slide,reviewers);
+            }else {
+                slide++;
+                changeText (slide,reviewers);
+            }
+        }
+    }
+
+    xhr.send();
+});
+
+
+prevButton.addEventListener('click', ()=> {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET','reviewers.json',true);
+
+    xhr.onload = function() {
+        if(this.status == 200) {
+            let data = this.responseText;
+            data = JSON.parse(data);
+            let reviewers = data.reviewers;
+            
+            if (slide == 0) {
+                slide = reviewers.length - 1;
+                changeText (slide,reviewers);
+            }else {
+                slide--;
+                changeText (slide,reviewers);
+            }
+        }
+    }
+
+    xhr.send();
+});
